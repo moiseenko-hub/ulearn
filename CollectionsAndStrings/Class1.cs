47350f6ab06adcc333c311c59d6256c040a95fc0
Andrey <andreymoiseenkohub@gmail.com>
2024-12-27 20:40:42 +0300

    add decode message and optimize contact methods

---
+++ CollectionsAndStrings/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace CollectionsAndStrings;
+
+public class Class1
+{
+    
+    private static string DecodeMessage(string[] lines)
+    {
+        var result = new List<string>();
+        foreach (var item in lines)
+        {
+            var splitString = item.Split();
+            foreach (var j in splitString)
+            {
+                if(!String.IsNullOrEmpty(j))
+                    if(Char.IsUpper(j[0]))
+                        result.Add(j);
+            }
+        }
+        result.Reverse();
+        return String.Join(" ", result);
+    }
+    
+    // Sа : { Sasha:sasha1995@sasha.ru, Sanya:alex99@mail.ru, Sasha:shurik2020@google.com }
+    
+    public static Dictionary<string, List<string>> OptimizeContacts(List<string> contacts)
+    {
+        var dictionary = new Dictionary<string, List<string>>();
+
+        foreach (var pair in contacts)
+        {
+            var splitElems = pair.Split(':');
+            var twoFirstSymbols = pair.Substring(0, 2).Replace(":", "");
+            if (dictionary.ContainsKey(twoFirstSymbols))
+            {
+                dictionary[twoFirstSymbols].Add(pair);
+            }
+            else
+            {
+                var temp = new List<string> { pair };
+                dictionary.Add(twoFirstSymbols, temp);
+            }
+        }
+        return dictionary;
+    }
+}
--- ulearn/Program.cs
+++ ulearn/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Enums;
+using Class1 = CollectionsAndStrings.Class1;
 
 namespace ulearn;
 
@@ -9,13 +10,33 @@ class Program
         //Console.WriteLine(Class1.GetOrderDescription(2));
         //Console.WriteLine(Class1.AccessRight.Guest.HasFlag(Class1.AccessRight.Delete));
         //Console.WriteLine(Arrays.ContainsAtIndex(new int[] { 1, 2, 3, 4, 5, 6 }, new[] { 3, 4, 5, 6 }, 2));
-        /*
-         * foreach (var item in Arrays.GetPoweredArray(new int[] { 1, 2, 3, 4, 5 }, 2))
-           {
-               Console.WriteLine(item);
-           }
-         */
-        
-        Matrix.Launch();
+        //Matrix.Launch();
+        //Matrix.SortLines(false);
+        //Matrix.Print();
+        var contacts = new List<string>
+        {
+            ":dimka212@gmail.com",
+            "Sasha:sasha1995@sasha.ru",
+            "Alex:alex99@mail.ru",
+            "Shurik:shurik2020@google.com",
+            "Sonia:sonia@example.com",
+            "Vanya:vanya123@mail.ru",
+            "Masha:masha1990@gmail.com",
+            "Sasha:sasha2000@gmail.com",
+            "Alex:alex2000@mail.ru",
+            "Sonia:sonia2021@yahoo.com",
+            "Dima:dima@example.com",
+            
+        };
+        foreach (var pair in Class1.OptimizeContacts(contacts))
+        {
+            Console.WriteLine(pair.Key);
+            foreach (var item in pair.Value)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
     }
+    
 }
